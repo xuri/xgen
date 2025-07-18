1902b48ef9cc3be208b8c84e12def36ed915e5eb
Matt Hinrichsen <matt.hinrichsen@workiva.com>
2025-07-18 15:07:29 -0500

    Support optional fields as pointers

--- genGo.go
+++ genGo.go
@@ -224,6 +224,9 @@ func (gen *CodeGenerator) GoComplexType(v *ComplexType) {
 			if fieldType == "time.Time" {
 				gen.ImportTime = true
 			}
+			if element.Optional && !strings.HasPrefix(fieldType, `*`) {
+				fieldType = "*" + fieldType
+			}
 			content += fmt.Sprintf("\t%s\t%s%s\t`xml:\"%s%s\"`\n", genGoFieldName(element.Name, false), plural, fieldType, element.Name, optional)
 		}
 		if len(v.Base) > 0 {
