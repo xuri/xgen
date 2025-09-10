986115c78d16882d5c9ed1a69ef9911214fe2d17
Josh Przybyszewski <josh.przybyszewski@workiva.com>
2025-09-10 11:42:05 -0500

    update the generated file

--- test/ts/base64.xsd.ts
+++ test/ts/base64.xsd.ts
@@ -57,7 +57,7 @@ export class MyType10 {
 export class TopLevel extends MyType6  {
 	CostAttr: number | null;
 	LastUpdatedAttr: string | null;
-	Nested: MyType7;
+	Nested?: MyType7;
 	MyType1: Uint8Array;
 	MyType2: Array<MyType2>;
 }
